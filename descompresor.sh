33629ed9065f2acf637fa66ff979112daf718d0c
ZarDer <victormgp92@gmail.com>
2021-05-11 12:59:27 +0200

    Update descompresor.sh

--- descompresor.sh
+++ descompresor.sh
@@ -7,23 +7,23 @@ nombre=$1
 ficherosDescomprimidos="El fichero se ha descomprimido en:"
 
 # Comprobación fichero de descompresion
-7z l $nombre 1>/dev/null 2>/dev/null
+7z l $nombre > /dev/null 2>&1
 
 
 if [ $? -eq 0 ]
 then
     # Si el código es 0, significará que el fichero se puede descomprimir por lo que cogeremos el fichero resultante
     # que nos aporta la opcion "l" de 7z para continuar comprobando si se puede descomprimir
-    ficheroDescomprimir=$(7z l $nombre 2>/dev/null | grep -A 2 "Name" | awk '(NR==3) {print $NF}')
+    ficheroDescomprimir=$(7z l $nombre > /dev/null 2>&1 | grep -A 2 "Name" | awk '(NR==3) {print $NF}')
 
     while  [ $ficheroDescomprimir ]
     do
         # Continuaremos descomprimiendo el fichero hasta que éste no pueda descomprimirse más
-        7z x $nombre 1>/dev/null 2>/dev/null
+        7z x $nombre > /dev/null 2>&1
 
         nombre=$fcheroDescomprimira
         ficherosDescomprimidos="$ficherosDescomprimidos ->$nombre"
-        ficheroDescomprimir=$(7z l $nombre 2>/dev/null | grep -A 2 "Name" | awk '(NR==3) {print $NF}')
+        ficheroDescomprimir=$(7z l $nombre > /dev/null 2>&1 | grep -A 2 "Name" | awk '(NR==3) {print $NF}')
 
     done
 
